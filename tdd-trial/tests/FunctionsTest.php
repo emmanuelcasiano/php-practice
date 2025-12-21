b6a62ef3996753ade0fbb6046fbdab7559f53ba4
EC <casianoemmanuel2217@gmail.com>
2025-12-22 04:12:24 +0800

    Functions test with proper function name

--- tdd-trial/tests/FunctionsTest.php
+++ tdd-trial/tests/FunctionsTest.php
@@ -8,8 +8,28 @@ require dirname(__DIR__) . '/lib/functions.php';
 
 final class FunctionsTest extends TestCase
 {
-    public function testAddIntegers(): void
+    public function testAddTwoPositiveIntegers(): void
     {
         $this->assertSame(5, addIntegers(2, 3));
+    }
+
+    public function testAddTwoNegativeIntegers(): void
+    {
+        $this->assertSame(-5, addIntegers(-2, -3));
+    }
+
+    public function testAddPositiveAndNegativeInteger(): void
+    {
+        $this->assertSame(1, addIntegers(3, -2));
+    }
+
+    public function testAddZeroToInteger(): void
+    {
+        $this->assertSame(3, addIntegers(3, 0));
+    }
+
+    public function testAddingIsCommutative(): void
+    {
+        $this->assertSame(addIntegers(3, 2), addIntegers(2, 3));
     }
 }
